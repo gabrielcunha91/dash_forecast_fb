c14e69c7e5dc2b9dffb167340764c39e627b8c41
Thais Risa Fujissaka <thais.fujissaka@gmail.com>
2025-04-09 19:13:39 -0300

    fix: bug de periodo sem a estimativa

--- utils/functions/dash_forecast.py
+++ utils/functions/dash_forecast.py
@@ -41,28 +41,35 @@ def idx_ultimo_dia_com_ticket_zig(df):
     return max(indices_ticket_zig)
 
 
-# Estimativa de Ticket para o primeiro mês
+# Estimativa de Ticket para o primeiro mes
 def df_estimativa_ticket_proximo_mes(df):
+
+    # Ultimo dia com dados reais
     idx_ultimo_dia_zig = idx_ultimo_dia_com_ticket_zig(df)
+    if idx_ultimo_dia_zig == None or idx_ultimo_dia_zig > df.index.max():
+        return df
     ultimo_dia_zig = datetime.datetime.strptime(df.loc[idx_ultimo_dia_zig, 'Data'], "%d-%m-%Y")
     mes_ultimo_dia_zig = ultimo_dia_zig.month
     
-    if idx_ultimo_dia_zig != None:
+    # Calcula o primeiro dia de estimativa
+    idx_primeiro_dia_estimativa = idx_ultimo_dia_zig + 1
+    if idx_primeiro_dia_estimativa > df.index.max():
+        return df
+    
+    primeiro_dia = datetime.datetime.strptime(df.loc[idx_primeiro_dia_estimativa, 'Data'], '%d-%m-%Y')
+    mes_primeiro_dia = primeiro_dia.month
 
-        idx_primeiro_dia_estimativa = idx_ultimo_dia_zig + 1
-        primeiro_dia = datetime.datetime.strptime(df.loc[idx_primeiro_dia_estimativa, 'Data'], '%d-%m-%Y')
-        mes_primeiro_dia = primeiro_dia.month
+    # Ultimo dia do mes
+    ultimo_dia = get_last_day_of_month(primeiro_dia)
+    num_estimativas = ultimo_dia - primeiro_dia.day
+    idx_ultimo_dia_estimativa = idx_primeiro_dia_estimativa + num_estimativas
 
-        ultimo_dia = get_last_day_of_month(primeiro_dia)
-
-        print(f"primeiro dia: {mes_primeiro_dia}, mes ultimo dia zig: {mes_ultimo_dia_zig}")
+    if idx_ultimo_dia_zig != None and idx_primeiro_dia_estimativa in df.index and idx_ultimo_dia_estimativa in df.index:
 
         if mes_primeiro_dia != mes_ultimo_dia_zig:
             return df
+        
         else:
-            num_estimativas = ultimo_dia - primeiro_dia.day
-            idx_ultimo_dia_estimativa = idx_primeiro_dia_estimativa + num_estimativas
-            
             # Verifica se o dataframe já possui a coluna 'Estimativa_Ticket'.
             if not ('Estimativa_Ticket' in df.columns):
                 df['Estimativa_Ticket'] = 0.0
